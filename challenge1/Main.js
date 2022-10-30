08f93e4c9c9d838b7be73c5c1efb117c85e13b5a
AAA4002911 <zeeshan4002911@gmail.com>
2022-10-30 23:38:41 +0530

    initial commit

---
+++ challenge1/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <ul>
+            <li>Mutiple webpage based Application: A web Application which have mutiple components spread in different pages of it</li>
+            <li>Single page Web Application like Facebook: A web Application which have Similar features like popular social media site Facebook, which works on single page arhitecture</li>
+            <li>Single Page E-commerce web Application: A web Application which has Product Listing, cart and buy feature</li>
+        </ul>
+    )
+}
+
+export default Main;
