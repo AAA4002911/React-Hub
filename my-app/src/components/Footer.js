3a98e152f1b5b8a06259bf8fedcba535abee0d0e
AAA4002911 <zeeshan4002911@gmail.com>
2022-10-30 23:48:07 +0530

    initial commit

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2022 web App development. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
