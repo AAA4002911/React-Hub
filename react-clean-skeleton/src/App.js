3340c3ec0f71bc6f46f8d1e9c630e4fe5beb4cdc
AAA4002911 <zeeshan4002911@gmail.com>
2024-05-12 16:43:41 +0530

    react clean skeleton added

--- react-clean-skeleton/src/App.js
+++ react-clean-skeleton/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import "./../styles/App.css";
+import "./App.css";
 
 function App() {
     return (
         <div id="main">
-            
+            React Clean Skeleton
         </div>
     )
 }
