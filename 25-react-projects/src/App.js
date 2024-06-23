ba2219cd4572511e05007664918bd17e564e3bd3
Md <zeeshan.md@accenture.com>
2024-06-23 09:48:06 +0530

    accordian added

---
+++ 25-react-projects/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+import Accordian from './components/accordian'
+
+function App() {
+    return (
+        <div className="App">
+            <Accordian/>
+        </div>
+    )
+}
+export default App;
